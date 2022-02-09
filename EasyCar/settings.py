1eda7c5185628b2d41f4e9bb273239a4884712fe
Konstantin-Never-Leave <90633138+Konstantin-Never-Leave@users.noreply.github.com>
2022-02-09 12:59:00 +0300

    Update settings.py

--- EasyCar/settings.py
+++ EasyCar/settings.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uz(rww=&qrk)manh_96ayxh5cn8r8+r8pj&+#jy!2&nfx-2^-n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
