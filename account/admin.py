7d456667abcd949cb5e353aecf66b92c0cff1291
Konstantin-Never-Leave <90633138+Konstantin-Never-Leave@users.noreply.github.com>
2022-02-24 10:03:21 +0300

    Add files via upload

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CustomUser, CommercialUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'second_name']
+    list_editable = ['first_name', 'second_name']
+
+
+@admin.register(CommercialUser)
+class CommercialUserAdmin(admin.ModelAdmin):
+    list_display = ['legal_name', 'owner', 'legal_address']
+    filter_horizontal = ['employees', 'service_list', 'serviced_car']
